3ede9f5783fe78550a23f8ef11d3dbd65bbbe4b9
sistr <game.sistr@gmail.com>
2021-12-17 14:08:47 +0900

    Forge版再度修正

--- forge/src/main/java/net/sistr/fgwithlm/forge/ForgeModEntryPoint.java
+++ forge/src/main/java/net/sistr/fgwithlm/forge/ForgeModEntryPoint.java
@@ -1,13 +1,15 @@
 package net.sistr.fgwithlm.forge;
 
 import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 import net.sistr.fgwithlm.FGWithLMMod;
+import thedarkcolour.kotlinforforge.forge.ForgeKt;
 
+@Mod(FGWithLMMod.MODID)
 public class ForgeModEntryPoint {
 
     public ForgeModEntryPoint() {
-        EventBuses.registerModEventBus(FGWithLMMod.MODID, FMLJavaModLoadingContext.get().getModEventBus());
+        EventBuses.registerModEventBus(FGWithLMMod.MODID, ForgeKt.getMOD_BUS());
         FGWithLMMod.init();
     }
 }
